71f5d83754e6b912ab3cc7ae3f7ad30431daedba
Marcelo Escobar <escobar@br.systems>
2020-05-20 15:31:52 -0300

    wip color slider

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ window.addEventListener('load', start);
 function start() {
   preventFormSubmit();
 
-  monitorSliders();
+  //monitorSliders();
 }
 
 function preventFormSubmit() {
@@ -15,6 +15,45 @@ function preventFormSubmit() {
   form.addEventListener('submit', handleFormSubmit);
 }
 
-function monitorSliders() {
+function updateSliders(id, value) {
+  function updateColor() {
+    r = document.getElementById('r');
+    g = document.getElementById('g');
+    b = document.getElementById('b');
+      
+    newColor = fullColorHex(r.value, g.value, b.value);
+    return newColor;
+  }
+
+  function updateCircleColor(newColor) {
+    circle = document.getElementById('circle');
+
+    newColor = '#' + newColor;
+    circle.style.backgroundColor = newColor;
+  }
+
+  sId = 's' + id;
+
+  slider = document.getElementById(id);
+  slider.value = value;
+  span = document.getElementById(sId);
+  span.innerText = value;
   
+  newColor = updateColor();
+  updateCircleColor(newColor);
+}
+
+var rgbToHex = function (rgb) { 
+  var hex = Number(rgb).toString(16);
+  if (hex.length < 2) {
+       hex = "0" + hex;
+  }
+  return hex;
+}
+
+var fullColorHex = function(r,g,b) {   
+  var red = rgbToHex(r);
+  var green = rgbToHex(g);
+  var blue = rgbToHex(b);
+  return red+green+blue;
 }
